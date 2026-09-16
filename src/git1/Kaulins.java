9d8a66b488811c297118acbacc4c1d3b244fb604
Herbert <robauteguilliman852@gmail.com>
2026-09-16 15:35:07 +0300

    Kauliņu var mest atkārtoti

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -24,13 +24,37 @@ public class Kaulins {
 
 	public static void main(String[] args) {
 		int reizes;
+		int izv;
 		Scanner scan = new Scanner(System.in);
-		do {
+		System.out.println();
+		System.out.print("1 - Mest kauliņu\n2 - Apturēt");
+		izv = scan.nextInt();
+		
+		switch(izv) {
+		
+		case 1:
+			do {
 			System.out.println("Cik reizes mest kauliņu?");
 			reizes = scan.nextInt();
-		}while(reizes<1);		
+			mestKaulinu(reizes);
+		}while(reizes>1 || izv!=2);
+				
+			break;
+		case 2: 
+			System.out.println("Programma ir apturēta!");
+			break;
+		}
+		
+		
+				
 		scan.close();
-		mestKaulinu(reizes);
+	
 	}
 	
     }
+	
+	
+	
+	
+
+
